eb614a0bae6add3a380f6189d80313aa738d919d
Dias Kozhabay <librerush@tutanota.com>
2019-11-04 23:02:56 +0600

    Add 1/2.pl

---
+++ 1/2.pl
@@ -0,0 +1,14 @@
+% exercise 1.2
+
+% loves(Vincent,mia) -> complex, arity 2
+% ’loves(Vincent,mia)’ -> atom
+% Butch(boxer) -> not a term
+% boxer(Butch) -> complex, arity 1 
+% and(big(burger),kahuna(burger)) -> complex, arity 2
+% and(big(X),kahuna(X)) -> complex, arity 2
+% _and(big(X),kahuna(X)) -> not a term
+% (Butch  kills  Vincent) -> not a term
+% kills(Butch  Vincent) -> not a term
+% kills(Butch,Vincent -> not a term
+
+
