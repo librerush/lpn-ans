90239d97800f9f69040df87fb0d164cf53414d96
Dias Kozhabay <librerush@tutanota.com>
2019-11-04 22:12:50 +0600

    Add 1/1.pl

---
+++ 1/1.pl
@@ -0,0 +1,15 @@
+% exercise 1.1
+
+
+% vINCENT -> atom
+% Footmassage -> var
+% variable23 -> atom
+% Variable2000 -> var
+% big_kahuna_burger -> atom
+% ’big  kahuna  burger’ -> atom
+% big  kahuna  burger -> neither
+% ’Jules’ -> atom
+% _Jules -> var
+% ’_Jules’ -> atom
+
+
