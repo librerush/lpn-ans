f36ba5ef78db1d568308eaa9f958e9b2f5eebb53
Dias Kozhabay <librerush@tutanota.com>
2020-01-06 02:22:55 +0600

    Add exercise 9.1

---
+++ 9/ex1.pl
@@ -0,0 +1,32 @@
+% exercise 9.1
+
+% ?- 12 is 2*6
+% true. 
+% ?- 14 =\= 2*6
+% true.
+% ?- 14 = 2*7
+% false.
+% ?- 14 == 2*7
+% false.
+% ?- 14 \== 2*7
+% true.
+% ?- 14 =:= 2*7
+% true.
+% ?- [1,2,3|[d,e]] == [1,2,3,d,e]
+% true.
+% ?- 2+3 == 3+2
+% false.
+% ?- 2+3 =:= 3+2
+% true. 
+% ?- 7-2 =\= 9-2
+% true.
+% ?- p == ’p’
+% true.
+% ?- p =\= ’p’
+% ERROR: Arithmetic: `p' is not a function
+% ?- vincent == VAR
+% false.
+% ?- vincent = VAR, VAR == vincent
+% true.
+% VAR = vincent.
+
